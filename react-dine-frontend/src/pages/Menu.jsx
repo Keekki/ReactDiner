99c18b254dafd39b0d9b966f22fa445e46eb2a06
Matias Frimodig <matias.frimodig@tuni.fi>
2023-12-06 13:12:27 +0200

    modified styling, styled the add to cart button, filtered the menu items to main dishes and desserts

--- react-dine-frontend/src/pages/Menu.jsx
+++ react-dine-frontend/src/pages/Menu.jsx
@@ -12,11 +12,41 @@ const Menu = () => {
       .catch((error) => console.error(error));
   }, []);
 
+  const mainDishes = [
+    "d1",
+    "d2",
+    "d3",
+    "d4",
+    "d5",
+    "d6",
+    "d7",
+    "d8",
+    "d9",
+    "d10",
+    "d11",
+    "d13",
+    "d14",
+    "d16",
+    "d17",
+    "d18",
+    "d20",
+  ];
+  const desserts = ["d12", "d15", "d19"];
+
   return (
     <div className="menu">
-      {items.map((item) => (
-        <MenuItem key={item.id} item={item} />
-      ))}
+      <h2 className="menu-header">Main dishes</h2>
+      {items
+        .filter((item) => mainDishes.includes(item.id))
+        .map((item) => (
+          <MenuItem key={item.id} item={item} />
+        ))}
+      <h2 className="menu-header">Desserts</h2>
+      {items
+        .filter((item) => desserts.includes(item.id))
+        .map((item) => (
+          <MenuItem key={item.id} item={item} />
+        ))}
     </div>
   );
 };
