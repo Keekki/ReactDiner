cfd5c4037c802a81183d206472324a3f056001b6
Matias Frimodig <matias.frimodig@tuni.fi>
2023-12-30 02:22:07 +0200

    small modification

--- react-dine-backend/index.js
+++ react-dine-backend/index.js
@@ -1,8 +1,20 @@
-const app = require('./app');
-require('dotenv').config();
+const app = require("./app");
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+// server/index.js
+const path = require("path");
+const express = require("express");
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../build")));
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../build", "index.html"));
 });
